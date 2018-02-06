1d4c1a34c47c1620efe4cf51c2a52093e2ae15ea
agedob <agedob888@gmail.com>
2018-02-06 09:59:39 -0800

    1 and done

---
+++ main.py
@@ -0,0 +1,8 @@
+def odd_even():
+    for i in range(1,2001,1):
+        if i % 2 == 0:
+            print i, "this is an even number"
+        else:
+            print i, "this numberr is odd"
+
+odd_even()
