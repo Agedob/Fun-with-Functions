8d59142e927fbd49d37664ede87a186844adda61
agedob <agedob888@gmail.com>
2018-02-06 12:19:51 -0800

    3rd times the charm

--- main.py
+++ main.py
@@ -8,5 +8,13 @@ def multiply(arr,num):
     for x in range(0,len(arr)):
         arr[x] *= num
     return arr
-a = [2,4,10,16]
-b = multiply(a, 5)
+def hacks(argu):
+    newarr=[]
+    for i in argu:
+        cont = []
+        for x in range(0, i, 1):
+            cont.append(1)
+        newarr.append(cont)
+    return newarr
+a = hacks(multiply([1,2,3],2))
+print a
